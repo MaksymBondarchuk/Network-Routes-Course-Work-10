0ff65ae8f9ea7acd7bad288b207f298911f37a71
MaxBondarchuk <bondarchuk.m.y@gmail.com>
2017-02-25 15:23:06 +0200

    With load from file

--- Network Routes Course Work (10)/Graph.cs
+++ Network Routes Course Work (10)/Graph.cs
@@ -1,20 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web.Script.Serialization;
 
 namespace Network_Routes_Course_Work_10
 {
     public class Graph
     {
         public List<List<int>> Matrix { get; set; } = new List<List<int>>();
-
-        public void LoadFromJson()
+        
+        public void LoadFromJson(string fileName)
         {
-            throw new NotImplementedException();
+            using (var file = new StreamReader(fileName))
+            {
+                var json = file.ReadToEnd();
+                var deserializer = new JavaScriptSerializer();
+                var result = deserializer.Deserialize<Graph>(json);
+                Matrix = result.Matrix;
+            }
         }
 
-        public void LoadToJson()
+        public void LoadToJson(string fileName)
         {
-            throw new NotImplementedException();
+            var json = new JavaScriptSerializer().Serialize(this);
+            using (var file = new StreamWriter(fileName))
+                file.WriteLine(json);
         }
     }
 }
--- Network Routes Course Work (10)/MainWindow.xaml.cs
+++ Network Routes Course Work (10)/MainWindow.xaml.cs
@@ -1,28 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Microsoft.Win32;
 
-namespace Network_Routes_Course_Work__10_
+namespace Network_Routes_Course_Work_10
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Graph Graph { get; set; } = new Graph();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new OpenFileDialog
+            {
+                DefaultExt = ".json",
+                Filter = "JavaScript Object Notation File|*.json"
+            };
+
+            if (dlg.ShowDialog() == true)
+                Graph.LoadFromJson(dlg.FileName);
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new SaveFileDialog
+            {
+                DefaultExt = ".json",
+                Filter = "JavaScript Object Notation File|*.json"
+            };
+
+            if (dlg.ShowDialog() == true)
+                Graph.LoadToJson(dlg.FileName);
         }
     }
 }
